db45e012e52f7b9f0a29bf7119f964ca0b5cab44
Zack <zmoumen13@gmail.com>
2018-03-27 17:57:41 -0600

    user placeholder

---
+++ models/User.js
@@ -0,0 +1,37 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    userName: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    email: { //users email
+      type: DataTypes.STRING,
+      // allowNull: false,
+      // validate: {
+      //   isEmail: true
+      // }
+    },
+    password: { //users password
+      type: DataTypes.STRING,
+//       validate: {
+//         len: [7, 25],
+//         isAlphanumeric: true
+//       }
+    },
+    image: { // users image (when we get to it)
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+
+
+  // User.associate = function(models) {
+  //   User.hasMany(models.Votes, {
+  //     onDelete: "cascade"
+  //   });
+  // };
+
+  return User;
+};
