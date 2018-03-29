1a6acd0889407031470aff47c25a4aeb8f352ca2
Zack <zmoumen13@gmail.com>
2018-03-29 10:42:43 -0600

    cleaned code and fixed bug to hit database

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,69 +1,21 @@
-//user vote api routes
+//api routes to database
 const db = require("../models");
-const yahooFinance = require('yahoo-finance');
-const Analysis = require("../models").Analysis;
 const router = require("express").Router();
 
 let ticker = "";
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-
-  // app.post("/api/ticker", function(req, res) {
-  //   //show all articles voted on
-  //   // console.log("---------------------");
-  //   // console.log(req.body.stock);
-  //   // console.log("---------------------");
-  //
-  //   ticker = req.body.stock;
-  //   ticker = ticker.toUpperCase();
-  //   console.log(ticker);
-  //   yahooFinance.quote({
-  //     symbol: ticker,
-  //     modules: ['price', 'summaryDetail', "defaultKeyStatistics"]       // optional; default modules.
-  //   }, function(err, quote) {
-  //     //console.log(quote);
-  //     //console.log(`Price = ${quote}`);
-  //     console.log(`forwardPE = ${quote.defaultKeyStatistics.forwardPE}`);
-  //
-  //
-  //     Analysis.create({
-  //       ticker: quote.price.symbol,
-  //       forwardPE: quote.price.regularMarketPrice,
-  //       price: quote.defaultKeyStatistics.forwardPE
-  //     }).then(function(data) {
-  //       console.log("data sent back to database");
-  //       res.json(data);
-  //     });
-  //
-  //   });
-  // });
-  //
-  // app.get("/api/:ticker", function(req, res) {
-  //   //show stocks searched by user
-  //   ticker = req.params.ticker;
-  //   ticker = ticker.toUpperCase();
-  //
-  //   yahooFinance.quote({
-  //     symbol: ticker,
-  //     modules: ['price', 'summaryDetail', "defaultKeyStatistics"]       // optional; default modules.
-  //   }, function(err, quote) {
-  //     if(err) {
-  //       console.log(err);
-  //     } else {
-  //       res.json(quote);
-  //     }
-  //   });
-  // });
 
   router.post("/api/favorite", (req, res) => {
-    console.log("api-route is being hit", req);
-    // db.Analysis.create({ticker: ticker})
-    // .then(function(data) {
-    //     console.log(data);
-    // })
-    // .catch(err => console.log(err))
+    console.log("------------------------------------");
+    console.log("api-route is being hit", req.body.ticker);
+    db.analyses.create({
+      ticker: req.body.ticker
+    }).then(function(data) {
+        console.log(data);
+    })
+    .catch(err => console.log(err))
   })
 
 
@@ -79,4 +31,6 @@ module.exports = function(app) {
   //     res.json(votes)
   //   })
   // })
-};
+// };
+
+module.exports = router;
