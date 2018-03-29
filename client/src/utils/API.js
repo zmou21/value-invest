6f9477f5a3aa1ba3b2e2c67cd429159b2567978e
Zack <zmoumen13@gmail.com>
2018-03-29 10:41:30 -0600

    updated code and commented it out

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,14 +1,12 @@
 import axios from "axios";
-import morgan from "morgan";
+// import morgan from "morgan";
 
-// The getRecipes method retrieves recipes from the server
-// It accepts a "query" or term to search the recipe api for
 export default {
+  //get route to grab stock data from API
   getStocksIEX: function(ticker) {
     return (
     axios
       .get("https://api.iextrading.com/1.0/stock/" + ticker + "/batch?types=quote,news,chart&range=1m&last=10")
-      // .then(console.log(req))
       .then(function(response) {
           //console.log(response.data.quote.companyName);
           return response;
@@ -16,11 +14,11 @@ export default {
       .catch(err => console.log(err))
     );
   },
+  //get route to grab logo from the API
   getStockLogo: function(ticker) {
     return (
     axios
       .get("https://api.iextrading.com/1.0/stock/" + ticker + "/logo")
-      // .then(console.log(req))
       .then(function(response) {
           //console.log(response.data.quote.companyName);
           return response;
@@ -28,18 +26,11 @@ export default {
       .catch(err => console.log(err))
     );
   },
+  //post route that allows a user to favorite a stock
   postFavorite: function(ticker) {
-
     return (
-      axios
-        .post("/api/favorite", {
-          ticker: ticker
-        })
-        .then(function(data){
-          console.log('api.js data', data);
-          return data;
-        })
-        .catch(err => console.log(err))
+      axios.post("/api/favorite", {ticker: ticker})
+      .catch(err => console.log(err))
     )
   }
 };
