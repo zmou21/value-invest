fe52ff7ebde3a216f1221c7f47cc158e9b447bab
Zack <zmoumen13@gmail.com>
2018-03-29 10:00:33 -0600

    added posting functionality

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import morgan from "morgan";
 
 // The getRecipes method retrieves recipes from the server
 // It accepts a "query" or term to search the recipe api for
@@ -14,5 +15,31 @@ export default {
       })
       .catch(err => console.log(err))
     );
+  },
+  getStockLogo: function(ticker) {
+    return (
+    axios
+      .get("https://api.iextrading.com/1.0/stock/" + ticker + "/logo")
+      // .then(console.log(req))
+      .then(function(response) {
+          //console.log(response.data.quote.companyName);
+          return response;
+      })
+      .catch(err => console.log(err))
+    );
+  },
+  postFavorite: function(ticker) {
+
+    return (
+      axios
+        .post("/api/favorite", {
+          ticker: ticker
+        })
+        .then(function(data){
+          console.log('api.js data', data);
+          return data;
+        })
+        .catch(err => console.log(err))
+    )
   }
 };
