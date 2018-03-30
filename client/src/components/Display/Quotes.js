178281e8d336eb851ed6a3ae64bbf70e5a6981ba
Zack <zmoumen13@gmail.com>
2018-03-30 16:11:27 -0600

    minor updates

--- client/src/components/Display/Quotes.js
+++ client/src/components/Display/Quotes.js
@@ -2,22 +2,17 @@ import React from "react";
 
 export const Quotes = props => (
   <div className="quote-list">
-    <h2>Quick Summary</h2>
     <ul className="left-list">
-      <li>Latest Price: {props.price}</li>
-      <li>Day-High: {props.high}</li>
-      <li>Day-Low: {props.low}</li>
-      <li>Price Change: {props.change}</li>
+      <li className="main-list-item">${props.price}</li>
+      <li className="less-list-item">High ${props.high} |</li>
+      <li className="less-list-item">Low ${props.low} |</li>
+      <li className="less-list-item">&Delta; ${props.change}</li>
     </ul>
     <ul className="middle-list">
-      <li>Market Cap: {props.marketCap}</li>
-      <li>Latest Volume: {props.volume}</li>
-      <li>52-Week-High: {props.yearHigh}</li>
-      <li>52-Week-Low: {props.yearLow}</li>
-    </ul>
-    <ul className="left-list">
-      <li>Sector: {props.sector}</li>
-      <li>Exchange: {props.exchange}</li>
+      <li className="main-list-item">Market Cap ${props.marketCap}</li>
+      <li className="less-list-item">Volume: {props.volume} |</li>
+      <li className="less-list-item">52-WH: ${props.yearHigh} |</li>
+      <li className="less-list-item">52-WL: ${props.yearLow}</li>
     </ul>
   </div>
 );
