f19093a4e4ddb6685d7066148832ba9ba3b47f1c
Zack <zmoumen13@gmail.com>
2018-04-01 15:17:49 -0600

    added get and post routes for yahoofinance npm

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 //api routes to database
 const db = require("../models");
 const router = require("express").Router();
+const yahooFinance = require('yahoo-finance');
 
 let ticker = "";
 
@@ -15,10 +16,28 @@ let ticker = "";
     }).then(function(data) {
         console.log(data);
     })
-    .catch(err => console.log(err))
-  })
+    .catch(err => console.log(err));
+  });
 
+  router.post("/api/ticker", (req, res) => {
+    console.log("------------------------------------");
+    console.log("api-route is being hit", req.body.ticker);
+    ticker = req.body.ticker;
 
+  });
+
+  router.get("/api/search", (req, res) => {
+    console.log("get route is connecting");
+    
+    yahooFinance.quote({
+      symbol: ticker,
+      modules: ['price', 'summaryDetail', "financialData", "defaultKeyStatistics"]       // optional; default modules.
+    }, function(err, quote) {
+      console.log(`General quote = ${quote}`);
+      console.log(`forwardPE = ${quote.defaultKeyStatistics.forwardPE}`);
+      res.json(quote);
+    });
+  });
 
   // app.put("/api/vote", function(req, res) {
   //   //to update the vote if need be
@@ -32,5 +51,16 @@ let ticker = "";
   //   })
   // })
 // };
+
+function yahooQuote(ticker) {
+  yahooFinance.quote({
+    symbol: ticker,
+    modules: ['price', 'summaryDetail', "financialData", "defaultKeyStatistics"]       // optional; default modules.
+  }, function(err, quote) {
+    console.log(`General quote = ${quote}`);
+    console.log(`forwardPE = ${quote.defaultKeyStatistics.forwardPE}`);
+    res.json(quote);
+  });
+}
 
 module.exports = router;
