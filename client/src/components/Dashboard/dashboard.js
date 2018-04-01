5bbc79daee595df2dc05a73048293927eb337d21
Zack <zmoumen13@gmail.com>
2018-04-01 15:15:14 -0600

    minor updates

--- client/src/components/Dashboard/dashboard.js
+++ client/src/components/Dashboard/dashboard.js
@@ -3,6 +3,8 @@ import React from "react";
 const Dashboard = props => (
   <div className="navbar">
     <h1 style={{color:"white"}}>ValuInvest</h1>
+    <h5>Quick Search (link here)</h5>
+    <h5>Deep Analysis Search</h5>
   </div>
 );
 
