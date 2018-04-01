d5ece6177edc45f260c052ca2f36d7dd96ff9f5b
Zack <zmoumen13@gmail.com>
2018-04-01 15:14:47 -0600

    added routes for analysis and quicksearch

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
 import QuickSearch from "./pages/QuickSearch";
+import Analysis from "./pages/Analysis";
 import Div from "./components/Dashboard";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => (
-  // <Router>
-  //   <div>
-  //     <Nav />
-  //     <Switch>
-  //       <Route exact path="/" component={Books}/>
-  //       <Route component={NoMatch}/>
-  //     </Switch>
-  //   </div>
-  // </Router>
-  <div className="grid-container">
-    <Div />
-    <QuickSearch />
-  </div>
+  <Router>
+    <div className="grid-container">
+      <Div />
+      <Switch>
+        <Route exact path="/" component={QuickSearch}/>
+        <Route exact path="/search" component={QuickSearch}/>
+        <Route exact path="/intrinsic" component={Analysis}/>
+      </Switch>
+    </div>
+  </Router>
+
 );
 
 export default App;
