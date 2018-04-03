f2318b9c39b689d98e671933866b3daf15b91a7d
Zack <zmoumen13@gmail.com>
2018-04-02 20:19:39 -0600

    updated

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -48,6 +48,12 @@ class Home extends Component {
                   toggleClick={this.toggle}
                 />
               </span>
+              <span id="loginbtn"><a onClick={this.toggle} className="linkstyle">login</a>
+                <LoginModal
+                  toggle={this.state.toggle}
+                  toggleClick={this.toggle}
+                />
+              </span>
           </div>
 
         </div>
