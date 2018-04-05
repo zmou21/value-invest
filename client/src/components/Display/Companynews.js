93a6570792b05649e77bcbfa47bc1d9fbb1531aa
Zack <zmoumen13@gmail.com>
2018-04-04 18:40:42 -0600

    updated with styling

--- client/src/components/Display/Companynews.js
+++ client/src/components/Display/Companynews.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export const Companynews = props => (
   <div className="company-news">
-    <h3 style={{display: "inline"}}>Company News</h3>
       {props.companyNews.map(news => (
         <ul className="news-list" key={news.url}>
+            <h5 style={{display: "inline", margin: "1%"}}>Company News</h5>
             <li className="description-item-title"><a href={news.url} target="_blank"><strong className="title-info">{news.headline}</strong></a></li>
-            <li className="description-item-news"><strong className="summary-info">Summary</strong>{news.summary}</li>
+            <li className="description-item-news"><strong className="summary-info">Summary:</strong><br/>{news.summary}</li>
             <li className="description-item-source">{news.source}</li>
         </ul>
       ))
