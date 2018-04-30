1c16c89cbd09427bd752265110e81a4fe717c932
Zack <zmoumen13@gmail.com>
2018-04-29 18:46:36 -0600

    add profile route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import QuickSearch from "./pages/QuickSearch";
 import Analysis from "./pages/Analysis";
+import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 // import Div from "./components/Dashboard";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -11,6 +12,7 @@ const App = () => (
         <Route exact path="/" component={Home}/>
         <Route exact path="/search" component={QuickSearch}/>
         <Route exact path="/intrinsic" component={Analysis}/>
+        <Route exact path="/profile" component={Profile}/>
       </Switch>
   </Router>
 
